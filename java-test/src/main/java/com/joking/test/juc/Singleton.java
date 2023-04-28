e0363592f39c7129775c806a362a5c2b9b3e6565
JOKING <jokinglove@foxmail.com>
2023-04-28 08:32:00 +0800

    update

--- java-test/src/main/java/com/joking/test/juc/Singleton.java
+++ java-test/src/main/java/com/joking/test/juc/Singleton.java
@@ -10,9 +10,13 @@ public class Singleton {
 
     private static Singleton singleton = null;
 
-    public static synchronized Singleton getInstance() {
+    public static Singleton getInstance() {
         if (singleton == null) {
-            singleton = new Singleton();
+            synchronized (Singleton.class) {
+                if (singleton == null) {
+                    singleton = new Singleton();
+                }
+            }
         }
         return singleton;
     }
